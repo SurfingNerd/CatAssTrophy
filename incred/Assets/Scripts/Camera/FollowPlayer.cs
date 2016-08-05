220744b01fd5e81fc1352c7623d33f0e9013fa8a
Thomas Haller <ThomasHaller@gmx.at>
2016-08-05 22:24:48 +0200

    Some test materials and a test scene

---
+++ incred/Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    [HideInInspector]
+    public Transform player;
+    [HideInInspector]
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        offset = transform.position;
+
+	}
+	
+	
+}
