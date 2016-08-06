0ba122086b5e6cc4e89857497f00c2082f5ae6d9
Thomas Haller <ThomasHaller@gmx.at>
2016-08-06 08:42:51 +0200

    ADD: Scroll support for Free Cam (Not working, to be fixed)

--- incred/Assets/Scripts/Camera/FreeCam.cs
+++ incred/Assets/Scripts/Camera/FreeCam.cs
@@ -9,6 +9,10 @@ public class FreeCam : MonoBehaviour {
 
     public float CameraMovementSpeed = 1;
 
+    public float CamDistanceMax = -10;
+    public float CamDistanceMin = -100;
+    public float CamDistanceChange = 1;
+
 	// Use this for initialization
 	void Start () {
         m_camera = GetComponent<Camera>();
@@ -26,9 +30,23 @@ public class FreeCam : MonoBehaviour {
 
         float y = Input.GetAxis("Vertical");
         float x = Input.GetAxis("Horizontal");
-
+        float camChange = Input.GetAxis("Mouse ScrollWheel");
         bool focusBall = Input.GetKey(KeyCode.Space);
 
+        if (camChange != 0)
+        {
+            float newZ = m_offset.z + camChange * CamDistanceChange;
+            if ( newZ > CamDistanceMax)
+            {
+                newZ = CamDistanceMax;
+            }
+            else if (newZ < CamDistanceMin)
+            {
+                newZ = CamDistanceMin;
+            }
+            m_offset = new Vector3(m_offset.x, m_offset.y, newZ);
+        }
+        
         if (focusBall)
         {
             transform.position = new Vector3(m_player.position.x + m_offset.x, m_player.position.y + m_offset.y, m_offset.z); 
@@ -36,8 +54,8 @@ public class FreeCam : MonoBehaviour {
         }
         else if (x != 0 || y != 0)
         {
-            transform.position = new Vector3(transform.position.x + x * CameraMovementSpeed, transform.position.y + y * CameraMovementSpeed, transform.position.z);
+            transform.position = new Vector3(transform.position.x + x * CameraMovementSpeed, transform.position.y + y * CameraMovementSpeed, m_offset.z);
         }
-        //
+    
 	}
 }
