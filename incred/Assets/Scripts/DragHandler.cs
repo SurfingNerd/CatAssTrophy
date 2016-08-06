a1eb060bc745c70502a2e6b80984b30d19f753fa
Johanna Pirker <joeyprink@gmail.com>
2016-08-07 00:38:32 +0200

    2d drag system

---
+++ incred/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public static GameObject itemBeingDragged;
+	Vector3 startPosition;
+	Transform startParent;
+
+	public Vector3          positionToReturnTo;
+
+
+
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		positionToReturnTo = this.transform.position;
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		Debug.Log ("OnBeginDrag");
+
+
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		this.transform.position = eventData.position;
+		Debug.Log ("OnDrag" + eventData.position);
+
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+		//this.transform.position = positionToReturnTo;
+		Debug.Log ("OnEndDrag");
+
+
+	}
+
+	#endregion
+
+
+
+}
