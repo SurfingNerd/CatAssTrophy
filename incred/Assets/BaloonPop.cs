6303a95a0f4b3727760b8a70e3142adb405913b6
Thomas Haller <ThomasHaller@gmx.at>
2016-08-09 18:08:27 +0200

    Proof of Concept: ExplodingBaloon

---
+++ incred/Assets/BaloonPop.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaloonPop : MonoBehaviour {
+
+    public GameObject baloonExplosionPrefab;
+    public float force = 1;
+    public float radius = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "baloonPopping")
+        {
+            if (baloonExplosionPrefab != null)
+            {
+                
+                GameObject newExplosion = Instantiate(baloonExplosionPrefab);
+                newExplosion.transform.position = transform.position;
+                //ExplodingBaloon explosionScript = newExplosion.GetComponent<ExplodingBaloon>();
+                Destroy(gameObject);
+
+                //if (explosionScript != null)
+                //{
+                //    explosionScript.force = force;
+                //    explosionScript.radius = radius;
+                    
+                //}
+                //else
+                //{
+                //    Debug.LogWarning(typeof(ExplodingBaloon).FullName + " not found on BaloonExplosionPrefab");
+                //}
+            }
+            else
+            {
+                Debug.LogWarning("BaloonExplosionPrefab needs to be set for " + GetType().FullName + " Script.");
+            }
+        }
+    }
+}
