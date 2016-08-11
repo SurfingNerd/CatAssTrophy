b5ddfca6a86c424527e50463a61c8313f523b3f1
Thomas Haller <ThomasHaller@gmx.at>
2016-08-11 12:40:52 +0200

    reorganized scripts into directories ExplodingBaloon: The next steps. + Test Scenes

---
+++ incred/Assets/Scripts/Gui/DropableObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropableObject : MonoBehaviour {
+
+    private Collider2D m_collider;
+    private Rigidbody2D m_rigidBody;
+
+	// Use this for initialization
+	void Start () {
+
+        m_collider.isTrigger = true;
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        m_collider.isTrigger = false;
+        m_rigidBody.isKinematic = true;
+    }
+
+    void Awake()
+    {
+        m_collider = GetComponent<Collider2D>();
+        m_rigidBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ incred/Assets/Scripts/Physics/DestroyMe.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyMe : MonoBehaviour {
+
+
+    public float afterseconds = 1;
+
+    private bool m_isDestroyed;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (!m_isDestroyed)
+        {
+            Destroy(gameObject, afterseconds);
+            m_isDestroyed = true;
+        }
+	}
+}
