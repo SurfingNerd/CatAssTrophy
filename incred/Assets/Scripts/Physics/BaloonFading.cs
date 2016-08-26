c964c87aae27f1952ee4bdb3ac9bf1d0213296db
Thomas Haller <ThomasHaller@gmx.at>
2016-08-26 20:52:04 +0200

    Baloon popping is now visualized.

---
+++ incred/Assets/Scripts/Physics/BaloonFading.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Physics
+{
+    public class BaloonFading : MonoBehaviour
+    {
+        private Renderer m_renderer;
+
+        public float FadeTime;
+
+        private float m_fadeTimeLeft;
+        private Vector3 m_initialScale;
+        void Start()
+        {
+            m_renderer = gameObject.GetComponent<Renderer>();
+            m_fadeTimeLeft = FadeTime;
+            m_initialScale = gameObject.transform.localScale;
+        }
+
+        void Update()
+        {
+            m_fadeTimeLeft = m_fadeTimeLeft - Time.deltaTime;
+            if (m_fadeTimeLeft < 0)
+            {
+                m_fadeTimeLeft = 0;
+            }
+
+            float alpha = m_fadeTimeLeft / FadeTime;
+            m_renderer.material.color = new Color(m_renderer.material.color.r, m_renderer.material.color.g, m_renderer.material.color.b, alpha);
+
+            float scaleMultiplier = 2 - alpha;
+            gameObject.transform.localScale = m_initialScale * scaleMultiplier;
+
+            if (m_fadeTimeLeft == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- incred/Assets/Scripts/Physics/BaloonPop.cs
+++ incred/Assets/Scripts/Physics/BaloonPop.cs
@@ -5,41 +5,63 @@ namespace Physics
 {
     public class BaloonPop : MonoBehaviour
     {
+        public BaloonPop()
+        {
+            ExplodingBaloonFadeTime = 0.25f;
+        }
+
         //public GameObject baloonExplosionPrefab;
         public float force = 1;
         public float radius = 1;
 
+        public float ExplodingBaloonFadeTime;
+        //private bool m_baloonIsPopped = false;
+
         void OnTriggerEnter2D(Collider2D other)
         {
-            if (enabled)
+            
+            if (other.tag == "baloonPopping")
             {
-                if (other.tag == "baloonPopping")
-                {
-                    //if (baloonExplosionPrefab != null)
+                //if (baloonExplosionPrefab != null)
+                //{
+
+                //GameObject newExplosion = Instantiate(baloonExplosionPrefab);
+                //newExplosion.transform.position = transform.position;
+                //ExplodingBaloon explosionScript = newExplosion.GetComponent<ExplodingBaloon>();
+
+                GameObject fadingBaloon = Instantiate<GameObject>(gameObject);
+                fadingBaloon.transform.SetParent(gameObject.transform.parent);
+                fadingBaloon.transform.position = gameObject.transform.position;
+
+                Collider2D collider = fadingBaloon.GetComponent<Collider2D>();
+                collider.enabled = false;
+
+                Rigidbody2D rigidBody = fadingBaloon.GetComponent<Rigidbody2D>();
+                rigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
+
+                BaloonFading fading = fadingBaloon.AddComponent<BaloonFading>();
+                fading.FadeTime = ExplodingBaloonFadeTime;
+
+                Destroy(gameObject);
+                //disable this behavior.
+                enabled = false;
+                    //if (explosionScript != null)
                     //{
+                    //    explosionScript.force = force;
+                    //    explosionScript.radius = radius;
 
-                        //GameObject newExplosion = Instantiate(baloonExplosionPrefab);
-                        //newExplosion.transform.position = transform.position;
-                        //ExplodingBaloon explosionScript = newExplosion.GetComponent<ExplodingBaloon>();
-                        Destroy(gameObject);
-
-                        //if (explosionScript != null)
-                        //{
-                        //    explosionScript.force = force;
-                        //    explosionScript.radius = radius;
-
-                        //}
-                        //else
-                        //{
-                        //    Debug.LogWarning(typeof(ExplodingBaloon).FullName + " not found on BaloonExplosionPrefab");
-                        //}
                     //}
                     //else
                     //{
-                    //    Debug.LogWarning("BaloonExplosionPrefab needs to be set for " + GetType().FullName + " Script.");
+                    //    Debug.LogWarning(typeof(ExplodingBaloon).FullName + " not found on BaloonExplosionPrefab");
                     //}
-                }
+                //}
+                //else
+                //{
+                //    Debug.LogWarning("BaloonExplosionPrefab needs to be set for " + GetType().FullName + " Script.");
+                //}
             }
+            
         }
     }
 }
